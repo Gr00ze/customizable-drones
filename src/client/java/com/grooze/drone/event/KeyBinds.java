6b99bbc05cb3b2f1a341a3300efbe176f99565ed
gr00ze <vincenzop23@gmail.com>
2024-11-12 21:10:13 +0100

    Added space keybind

--- src/client/java/com/grooze/drone/event/KeyBinds.java
+++ src/client/java/com/grooze/drone/event/KeyBinds.java
@@ -9,20 +9,36 @@ import org.lwjgl.glfw.GLFW;
 public class KeyBinds {
 
     public static KeyBinding
-            droneUP = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.fabricmod.droneup", // Identificatore del binding
-            InputUtil.Type.KEYSYM, // Tipo di input
-            GLFW.GLFW_KEY_SPACE, // Tasto associato (G in questo caso)
-            "category.fabricmod.drone" // Categoria nelle impostazioni
-    )),
-            droneDown = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.fabricmod.dronedown", // Identificatore del binding
-            InputUtil.Type.KEYSYM, // Tipo di input
-            GLFW.GLFW_KEY_LEFT_CONTROL, // Tasto associato (G in questo caso)
-            "category.fabricmod.drone" // Categoria nelle impostazioni
-    ));
+            droneUP,
+            droneDown,
+            spaceKeyBinding;
 
-    public static void init(){}
+    public static void init(){
+        registerKeyBinding();
+    }
+
+    private static void registerKeyBinding() {
+        droneUP = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.fabricmod.droneup", // Identificatore del binding
+                InputUtil.Type.KEYSYM, // Tipo di input
+                GLFW.GLFW_KEY_SPACE, // Tasto associato (G in questo caso)
+                "category.drone.controls" // Categoria nelle impostazioni
+        ));
+        droneDown = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.fabricmod.dronedown", // Identificatore del binding
+                InputUtil.Type.KEYSYM, // Tipo di input
+                GLFW.GLFW_KEY_LEFT_CONTROL, // Tasto associato (G in questo caso)
+                "category.drone.controls" // Categoria nelle impostazioni
+        ));
+
+        spaceKeyBinding =  KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.drone.up", // Identificatore del binding
+                InputUtil.Type.KEYSYM, // Tipo di input
+                GLFW.GLFW_KEY_SPACE, // Tasto associato (G in questo caso)
+                "category.drone.controls" // Categoria nelle impostazioni
+        ));
+
+    }
 
 
 }
